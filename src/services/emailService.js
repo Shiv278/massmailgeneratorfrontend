ff10a19699635622f475440d54362e64d23d910f
Shivani <shivaninaik070@gmail.com>
2024-12-26 03:35:40 +0530

    change backend url

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/send-email';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080/send-email';
 
 export const sendEmail = async (emailData) => {
     try {
